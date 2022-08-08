88e0ff9e81cf7e8d9be14c7320818666b8bf226b
Prabhath <penprabh@publicisgroupe.net>
2022-08-08 13:01:14 +0530

    updated script

--- script/octoperf-pipeline.groovy
+++ script/octoperf-pipeline.groovy
@@ -16,9 +16,9 @@ node {
     }
 
 	stage('Jmeter'){
-		steps{
+		
 			echo 'Jmeter'
-		}
+		
 	}
 	stage('Execute Performance Tests') {
         dir("${WORKSPACE}/test plan") {
@@ -26,33 +26,33 @@ node {
         }
       }
 	stage('Jmeter test file'){
-		steps{
+		
 			dir("${WORKSPACE}/test plan") {
 			bat 'del Shift-Left.jtl'
 			bat 'C:\\Training\\Jmeter\\apache-jmeter-5.5\\bin\\jmeter.bat -n -t Test_Plan3.jmx -l Shift-Left.jtl'
 			 }
-		}
+		
 	}
 	stage('test response time result'){
-		steps{
+		
 			script{
 			perfReport errorFailedThreshold: 1, errorUnstableResponseTimeThreshold: 'Shift-Left.jtl:20', filterRegex: '', modePerformancePerTestCase: true, showTrendGraphs: true, sourceDataFiles: 'Shift-Left.jtl'
 			echo "${currentBuild.result}"
 			}
-		}
+		
 	}
 	stage('Jmeter generate csv'){
-		steps{
+		
 			script{
 			
 			bat 'del test1.csv'
 			bat 'C:\\Training\\Jmeter\\apache-jmeter-5.5\\bin\\JMeterPluginsCMD.bat --generate-csv test1.csv --input-jtl Shift-Left.jtl --plugin-type SynthesisReport'
 			
 			}
-		}
+		
 	}
 	stage('test throughput value'){
-		steps {
+		
 			script{
 			
 				def records = readFile file: 'test1.csv'
@@ -68,6 +68,6 @@ node {
 				}
 			}
 		
-		}
+		
 	}
 }
