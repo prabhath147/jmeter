5e4a22f0fe575d2b75e4d967ef341afdeba007ed
Prabhath <penprabh@publicisgroupe.net>
2022-08-08 17:06:40 +0530

    updated script

--- script/octoperf-pipeline.groovy
+++ script/octoperf-pipeline.groovy
@@ -1,6 +1,9 @@
 pipeline {
     agent any
-
+	environment {
+		OUTPUT_PATH="C:\\Training\\Jmeter\\jmeter\\test_plan"
+		
+	}
 	stages{
     stage('Initialise') {
 	steps{
@@ -30,14 +33,14 @@ pipeline {
 		steps{
 			dir("${WORKSPACE}\\test_plan") {
 			//bat 'del C:\\Training\\Jmeter\\jmeter\\test_plan\\Shift-Left.jtl'
-			bat 'C:\\Training\\Jmeter\\apache-jmeter-5.5\\bin\\jmeter.bat -n -t Test_Plan3.jmx -l C:\\Training\\Jmeter\\jmeter\\test_plan\\Shift-Left.jtl'
+			bat 'C:\\Training\\Jmeter\\apache-jmeter-5.5\\bin\\jmeter.bat -n -t Test_Plan3.jmx -l ${OUTPUT_PATH}\\Shift-Left.jtl'
 			 }
 		}
 	}
 	stage('test response time result'){
 		steps{
 			script{
-			perfReport errorFailedThreshold: 10, errorUnstableResponseTimeThreshold: 'Shift-Left.jtl:20', filterRegex: '', modePerformancePerTestCase: true, showTrendGraphs: true, sourceDataFiles: 'C:\\Training\\Jmeter\\jmeter\\test_plan\\Shift-Left.jtl'
+			perfReport errorFailedThreshold: 10, errorUnstableResponseTimeThreshold: 'Shift-Left.jtl:20', filterRegex: '', modePerformancePerTestCase: true, showTrendGraphs: true, sourceDataFiles: ${OUTPUT_PATH}\\Shift-Left.jtl'
 			echo "${currentBuild.result}"
 			}
 		}
@@ -46,7 +49,7 @@ pipeline {
 		steps{
 			script{			
 			//bat 'del C:\\Training\\Jmeter\\jmeter\\test_plan\\test1.csv'
-			bat 'C:\\Training\\Jmeter\\apache-jmeter-5.5\\bin\\JMeterPluginsCMD.bat --generate-csv C:\\Training\\Jmeter\\jmeter\\test_plan\\test1.csv --input-jtl C:\\Training\\Jmeter\\jmeter\\test_plan\\Shift-Left.jtl --plugin-type SynthesisReport'
+			bat 'C:\\Training\\Jmeter\\apache-jmeter-5.5\\bin\\JMeterPluginsCMD.bat --generate-csv ${OUTPUT_PATH}\\test1.csv --input-jtl ${OUTPUT_PATH}\\Shift-Left.jtl --plugin-type SynthesisReport'
 			
 			}
 		}
@@ -55,7 +58,7 @@ pipeline {
 		steps{
 			script{
 			
-				def records = readFile file: 'C:\\Training\\Jmeter\\jmeter\\test_plan\\test1.csv'
+				def records = readFile file: ${OUTPUT_PATH}'\\test1.csv'
 				def lines=records.readLines()
 			   
 				def lastline=lines.get(lines.size()-1).split(",")    
@@ -73,8 +76,8 @@ pipeline {
 		steps{
 			dir("${WORKSPACE}\\test_plan") {
 			
-			bat 'del C:\\Training\\Jmeter\\jmeter\\test_plan\\Shift-Left.jtl'
-			bat 'del C:\\Training\\Jmeter\\jmeter\\test_plan\\test1.csv'
+			bat 'del ${OUTPUT_PATH}\\Shift-Left.jtl'
+			bat 'del ${OUTPUT_PATH}\\test1.csv'
 			 }
 		}
 	}
